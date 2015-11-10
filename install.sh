682552cd0a18e546d7724cae63d5d9ba5f6c6fa8
John Grossmann <jgrossmann@colgate.edu>
2015-11-10 02:39:13 -0500

    lskdjf

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-chown user01:user01 ./addqueue
-chown user01:user01 ./showqueue
-chown user01:user01 /bin/rmqueue
-
-chmod u+s ./addqueue
-chmod u+s ./showqueue
-chmod u+s /bin/rmqueue
-
 cp ./addqueue /bin/
 cp ./showqueue /bin/
 cp ./rmqueue /bin/
+
+chown user01:user01 /bin/addqueue
+chown user01:user01 /bin/showqueue
+chown user01:user01 /bin/rmqueue
+
+chmod u+s /bin/addqueue
+chmod u+s /bin/showqueue
+chmod u+s /bin/rmqueue
+
