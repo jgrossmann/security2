a482f20ca6073da305b1e930242cb424248920aa
John Grossmann <jgrossmann@colgate.edu>
2015-11-10 02:35:32 -0500

    updslfj

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-chown user01:user01 addqueue
-chown user01:user01 showqueue
-chown user01:user01 rmqueue
+chown user01:user01 ./addqueue
+chown user01:user01 ./showqueue
+chown user01:user01 ./rmqueue
 
-chmod u+s addqueue
-chmod u+s showqueue
-chmod u+s rmqueue
+chmod u+s ./addqueue
+chmod u+s ./showqueue
+chmod u+s ./rmqueue
 
-cp addqueue /bin/
-cp showqueue /bin/
-cp rmqueue /bin/
+cp ./addqueue /bin/
+cp ./showqueue /bin/
+cp ./rmqueue /bin/
